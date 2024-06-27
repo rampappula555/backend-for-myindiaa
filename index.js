fd4d322f323a16afcf6ef041f71e05a85416730f
ram@555 <rampappula555@gmail.com>
2024-06-27 10:48:29 +0530

    added access controlled allowed header

--- index.js
+++ index.js
@@ -4,7 +4,14 @@ const dotenv = require("dotenv");
 const cors = require("cors");
 const app = express();
 app.use(express.json());
-app.use(cors());
+// app.use(cors());
+app.use(
+  cors({
+    origin: "http://localhost:3000", // or your specific frontend URL
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
 dotenv.config();
 
 const port = process.env.PORT || 5500;
